cc9126eed7a9ba6ef08cd13b96736c9bd3ef688c
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-09 16:12:01 +0100

    Change attributes format

--- db/migrate/20210109131334_create_bookings.rb
+++ db/migrate/20210109131334_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.integer :start_date
-      t.integer :end_date
+      t.datetime :start_date
+      t.datetime :end_date
       t.references :planet, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
