7cdf211364085b38158963555c41d4c015008327
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-09 14:54:26 +0100

    Created Planets Controller

---
+++ app/controllers/planets_controller.rb
@@ -0,0 +1,24 @@
+class PlanetsController < ApplicationController
+  def index
+    @planets = Planet.all
+  end
+
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(planets_params)
+    if @planet.save
+      redirect_to planets_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def planet_params
+    params.require(:planet).permit(:name, :description, :address, :price, :number_guests)
+  end
+end
