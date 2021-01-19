9d2f1be2d70a650c5c41a480a8415cb49c06cdbf
Alexandre Didion <ale.didion@gmail.com>
2021-01-19 16:10:23 +0100

    Add new column to User tables : description and also avatar with active storage ; create my profile page ; changed the edit possibilities

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -37,7 +37,7 @@ class PlanetsController < ApplicationController
 
   def update
     if @planet.update(planet_params)
-      redirect_to planets_path, notice: 'Planet is updated'
+      redirect_to planet_path(@planet), notice: 'Planet is updated'
     else
       render :edit
     end
@@ -57,5 +57,4 @@ class PlanetsController < ApplicationController
   def set_planet
     @planet = Planet.find(params[:id])
   end
-
 end
