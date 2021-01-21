bb8e3a3268a5e5047d88738d8ea24fb26eebda25
Laurene Eppelle <laurene.eppelle@gmail.com>
2021-01-21 19:51:22 +0100

    add top planets to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @planets = Planet.all.sort_by {|planet| planet.bookings.size}.take(5)
   end
 end
